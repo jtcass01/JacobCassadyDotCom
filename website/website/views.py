84efb62188224008b9bfb708cd2cab8051bbfec3
Jacob Taylor Cassady <jtcass01@louisville.edu>
2019-03-08 02:20:42 -0500

    Moving over previoius stuff into new website

--- website/website/views.py
+++ website/website/views.py
@@ -16,22 +16,62 @@ def home():
         year=datetime.now().year,
     )
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
 @app.route('/about')
 def about():
     """Renders the about page."""
     return render_template(
         'about.html',
-        title='About',
+        title='About Me',
         year=datetime.now().year,
-        message='Your application description page.'
+        message=''
+    )
+
+@app.route('/projects')
+def projects():
+    """Renders the projects page."""
+    return render_template(
+        'projects.html',
+        title='Projects',
+        year=datetime.now().year,
+        message=''
+    )
+
+@app.route('/awards')
+def awards():
+    """Renders the awards page."""
+    return render_template(
+        'awards.html',
+        title='Awards',
+        year=datetime.now().year,
+        message=''
+    )
+
+@app.route('/transcripts')
+def transcripts():
+    """Renders the transcripts page."""
+    return render_template(
+        'transcripts.html',
+        title='Transcripts',
+        year=datetime.now().year,
+        message=''
+    )
+
+@app.route('/work_history')
+def work_history():
+    """Renders the transcripts page."""
+    return render_template(
+        'work_history.html',
+        title='Work History',
+        year=datetime.now().year,
+        message=''
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='',
+        year=datetime.now().year,
+        message=''
     )
