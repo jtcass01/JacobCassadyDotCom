5e5fbc3963d53f52d95833b49089777bce56899e
Jacob Cassady <jtcass01@louisville.edu>
2019-03-12 11:05:02 -0400

    Updating transcript page

---
+++ website/website/static/scripts/transcripts.js
@@ -0,0 +1,120 @@
+function show_fall_2013() {
+    var text_field = document.getElementById("2013-fall");
+
+    if (text_field.style.display === "none") {
+        text_field.style.display = "block";
+    } else {
+        text_field.style.display = "none";
+    }
+}
+
+function show_spring_2014() {
+    var text_field = document.getElementById("2014-spring");
+
+    if (text_field.style.display === "none") {
+        text_field.style.display = "block";
+    } else {
+        text_field.style.display = "none";
+    }
+}
+
+function show_summer_2014() {
+    var text_field = document.getElementById("2014-summer");
+
+    if (text_field.style.display === "none") {
+        text_field.style.display = "block";
+    } else {
+        text_field.style.display = "none";
+    }
+}
+
+function show_fall_2014() {
+    var text_field = document.getElementById("2014-fall");
+
+    if (text_field.style.display === "none") {
+        text_field.style.display = "block";
+    } else {
+        text_field.style.display = "none";
+    }
+}
+
+function show_spring_2015() {
+    var text_field = document.getElementById("2015-spring");
+
+    if (text_field.style.display === "none") {
+        text_field.style.display = "block";
+    } else {
+        text_field.style.display = "none";
+    }
+}
+
+function show_fall_2015() {
+    var text_field = document.getElementById("2015-fall");
+
+    if (text_field.style.display === "none") {
+        text_field.style.display = "block";
+    } else {
+        text_field.style.display = "none";
+    }
+}
+
+function show_spring_2016() {
+    var text_field = document.getElementById("2016-spring");
+
+    if (text_field.style.display === "none") {
+        text_field.style.display = "block";
+    } else {
+        text_field.style.display = "none";
+    }
+}
+
+function show_summer_2016() {
+    var text_field = document.getElementById("2016-summer");
+
+    if (text_field.style.display === "none") {
+        text_field.style.display = "block";
+    } else {
+        text_field.style.display = "none";
+    }
+}
+
+function show_fall_2016() {
+    var text_field = document.getElementById("2016-fall");
+
+    if (text_field.style.display === "none") {
+        text_field.style.display = "block";
+    } else {
+        text_field.style.display = "none";
+    }
+}
+
+function show_summer_2017() {
+    var text_field = document.getElementById("2017-summer");
+
+    if (text_field.style.display === "none") {
+        text_field.style.display = "block";
+    } else {
+        text_field.style.display = "none";
+    }
+}
+
+function show_spring_2018() {
+    var text_field = document.getElementById("2018-spring");
+
+    if (text_field.style.display === "none") {
+        text_field.style.display = "block";
+    } else {
+        text_field.style.display = "none";
+    }
+}
+
+
+function show_fall_2018() {
+    var text_field = document.getElementById("2018-fall");
+
+    if (text_field.style.display === "none") {
+        text_field.style.display = "block";
+    } else {
+        text_field.style.display = "none";
+    }
+}
