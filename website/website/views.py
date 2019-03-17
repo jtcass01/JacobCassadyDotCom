f1e06b79e29abf941d06778b7bcbd078a10280a1
Jacob T Cassady <jtcass01@louisville.edu>
2019-03-17 08:40:16 -0400

    Added google analytics and updated about page.

--- website/website/views.py
+++ website/website/views.py
@@ -2,14 +2,19 @@
 Routes and views for the flask application.
 """
 
+import os
 from datetime import datetime
-from flask import render_template
+from flask import render_template, send_file
 from website import app
+from website.google_analytics import track_event
 
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
+    track_event(
+		category='home',
+		action='home page visit')
     return render_template(
         'index.html',
         title='Home Page',
@@ -19,6 +24,9 @@ def home():
 @app.route('/about')
 def about():
     """Renders the about page."""
+    track_event(
+		category='about',
+		action='about page visit')
     return render_template(
         'about.html',
         title='About Me',
@@ -29,6 +37,9 @@ def about():
 @app.route('/projects')
 def projects():
     """Renders the projects page."""
+    track_event(
+		category='projects',
+		action='projects page visit')
     return render_template(
         'projects.html',
         title='Projects',
@@ -39,6 +50,9 @@ def projects():
 @app.route('/simon')
 def simon():
     """Renders the simon project page."""
+    track_event(
+		category='projects',
+		action='simon page visit')
     return render_template(
         'simon.html',
         title='Sign Interfaced Machine Operating Network (SIMON)',
@@ -49,6 +63,9 @@ def simon():
 @app.route('/awards_and_certifications')
 def awards_and_certifications():
     """Renders the awards page."""
+    track_event(
+		category='awards_and_certifications',
+		action='awards_and_certifications page visit')
     return render_template(
         'awards_and_certifications.html',
         title='Awards and Certifications',
@@ -59,6 +76,9 @@ def awards_and_certifications():
 @app.route('/transcripts')
 def transcripts():
     """Renders the transcripts page."""
+    track_event(
+		category='transcripts',
+		action='transcripts page visit')
     return render_template(
         'transcripts.html',
         title='University Transcripts',
@@ -69,6 +89,9 @@ def transcripts():
 @app.route('/work_history')
 def work_history():
     """Renders the transcripts page."""
+    track_event(
+		category='work_history',
+		action='work_history page visit')
     return render_template(
         'work_history.html',
         title='Work History',
@@ -79,9 +102,55 @@ def work_history():
 @app.route('/contact')
 def contact():
     """Renders the contact page."""
+    track_event(
+		category='contact',
+		action='contact page visit')
     return render_template(
         'contact.html',
         title='Contact Info',
         year=datetime.now().year,
         message=''
     )
+
+''' Dean's List Certificate Routes '''
+@app.route('/dean_cert_fall_2013')
+def dean_cert_fall_2013():
+	track_event(
+		category='contact',
+		action='contact page visit')
+	return send_file(os.getcwd() + os.path.sep + 'website' + os.path.sep + 'static' + os.path.sep + 'transcripts' + os.path.sep + '2013' + os.path.sep + 'dean_cert_fall_2013.pdf')
+
+@app.route('/dean_cert_spring_2014')
+def dean_cert_spring_2014():
+	track_event(
+		category='contact',
+		action='contact page visit')
+	return send_file(os.getcwd() + os.path.sep + 'website' + os.path.sep + 'static' + os.path.sep + 'transcripts' + os.path.sep + '2014' + os.path.sep + 'dean_cert_spring_2014.pdf')
+
+@app.route('/dean_cert_fall_2015')
+def dean_cert_fall_2015():
+	track_event(
+		category='contact',
+		action='contact page visit')
+	return send_file(os.getcwd() + os.path.sep + 'website' + os.path.sep + 'static' + os.path.sep + 'transcripts' + os.path.sep + '2015' + os.path.sep + 'dean_cert_fall_2015.pdf')
+
+@app.route('/dean_cert_fall_2016')
+def dean_cert_fall_2016():
+	track_event(
+		category='contact',
+		action='contact page visit')
+	return send_file(os.getcwd() + os.path.sep + 'website' + os.path.sep + 'static' + os.path.sep + 'transcripts' + os.path.sep + '2016' + os.path.sep + 'dean_cert_fall_2016.pdf')
+
+@app.route('/dean_cert_spring_2018')
+def dean_cert_spring_2018():
+	track_event(
+		category='contact',
+		action='contact page visit')
+	return send_file(os.getcwd() + os.path.sep + 'website' + os.path.sep + 'static' + os.path.sep + 'transcripts' + os.path.sep + '2018' + os.path.sep + 'dean_cert_spring_2018.pdf')
+
+@app.route('/dean_cert_fall_2018')
+def dean_cert_fall_2018():
+	track_event(
+		category='contact',
+		action='contact page visit')
+	return send_file(os.getcwd() + os.path.sep + 'website' + os.path.sep + 'static' + os.path.sep + 'transcripts' + os.path.sep + '2018' + os.path.sep + 'dean_cert_fall_2018.pdf')
